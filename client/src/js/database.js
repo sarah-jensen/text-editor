c9c81f7c81b659049c78421ffb1e819d17699caf
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-06-17 17:14:19 -0700

    add putDb and getDb logic to client/src/js/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
+const initdb = async () => 
   openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
@@ -13,9 +13,53 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  //connect to correct version of the database
+  const contactDb = await openDB('jate', 1); 
+
+  //create new transaction with jate database with read-write privileges
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  //open object store
+  const store = tx.objectStore('jate');
+
+  //pass in new content
+  const request = store.add(
+    {
+      content,
+    });
+
+  // confirm request
+  const result = await request;
+  console.log('content saved to database', result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+console.error('getDb not implemented');
+export const getDb = async () => {
+  
+  //connect to database and version
+  const contactDb = await openDB('jate', 1);
+  
+//create new transaction, specify database and privileges
+const tx = contactDb.transaction('jate', 'readonly');
 
+//open the object store
+const store = tx.objectStore('jate');
+
+//use .getAll() to get all data in the database
+const request = store.getAll();
+
+//confirm request
+const result = await request;
+console.log('result.value', result);l
+return result;
+};
+
+
+
+//start the database
 initdb();
