a533dfcf30ac7041e6b897a960a8c4c8d0ae3407
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-06-18 14:08:08 -0700

    correct typos and formatting for database.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -48,13 +48,10 @@ module.exports = () => {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: 
-              [
-                '@babel/preset-env'
-              ],
-              plugins: 
-              [
-                '@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'
+              presets: ['@babel/preset-env'],
+              plugins: [
+                '@babel/plugin-proposal-object-rest-spread', 
+                '@babel/transform-runtime'
               ],
             }
           }
