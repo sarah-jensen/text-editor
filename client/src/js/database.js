d9d2e77598d0c541532e6d4b6d0489158b6975b1
Siofra <122956133+sarah-jensen@users.noreply.github.com>
2023-06-18 17:25:37 -0700

    update event handlers for install button

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,52 +14,49 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
-
+  
   //connect to correct version of the database
   const jateDb = await openDB('jate', 1); 
-
+  
   //create new transaction with jate database with read-write privileges
   const tx = jateDb.transaction('jate', 'readwrite');
-
+  
   //open object store
   const store = tx.objectStore('jate');
-
+  
   //pass in new content
-  const request = store.add(
+  const request = store.put(
     {
-      content,
+      text: content,
     });
-
-  // confirm request
-  const result = await request;
-  console.log('content saved to database', result);
-};
+    
+    // confirm request
+    const result = await request;
+    console.log('content saved to database', result);
+    return result;
+  };
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
   
   //connect to database and version
   const jateDb = await openDB('jate', 1);
   
-//create new transaction, specify database and privileges
-const tx = jateDb.transaction('jate', 'readonly');
-
-//open the object store
-const store = tx.objectStore('jate');
-
-//use .getAll() to get all data in the database
-const request = store.getAll();
-
-//confirm request
-const result = await request;
-console.log('result.value', result);
-return result;
+  //create new transaction, specify database and privileges
+  const tx = jateDb.transaction('jate', 'readonly');
+  
+  //open the object store
+  const store = tx.objectStore('jate');
+  
+  //use .getAll() to get all data in the database
+  const request = store.getAll();
+  
+  //confirm request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
-
-
 
 // call the initialize database function
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,19 +4,25 @@ const textHeader = document.getElementById('textHeader');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
+    console.log('is it working?')
+    window.deferredPrompt = event;
     butInstall.style.visibility = 'visible';
     
     // TODO: Implement a click event handler on the `butInstall` element
     butInstall.addEventListener('click', () => {
+        console.log('install button')
         event.prompt();
         butInstall.setAttribute('disabled', true);
         butInstall.textContent = 'Installed!';
+
+        butInstall.classList.toggle('hidden', true);
     });
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    textHeader.textContent = 'Just Another Text Editor: Successfully Installed!';
+    console.log('line 24')
+    window.deferredPrompt = null;
+    textHeader.textContent = 'Just Another Text Editor: Installed';
     console.log('appinstalled', event);
 });
